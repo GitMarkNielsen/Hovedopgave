f9c9de476b01d29f603777b68ff77b5ca65c1427
GitMarkNielsen <Mark-Nielsen@outlook.com>
2025-12-09 14:20:59 +0100

    Started Writing tests

---
+++ Project/Program/Tests/CategoryDBO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Tests
+{
+    internal class CategoryDBO
+    {
+        public string ItemGroupName { get; set; }
+        public int PriceMin { get; set; }
+        public int PriceMax { get; set; }
+        public string[] Sizes { get; set; }
+
+        public CategoryDBO(string ItemGroupName, int PriceMin, int PriceMax, string[] Sizes) {
+            this.ItemGroupName = ItemGroupName;
+            this.PriceMin = PriceMin;
+            this.PriceMax = PriceMax;
+            this.Sizes = Sizes;
+        }
+    }
+    
+}
---
+++ Project/Program/Tests/FictionalProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Tests
+{
+    internal class FictionalProduct
+    {
+        public int EAN { get; set; }
+        public string ItemGroupName { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+
+        public FictionalProduct(string ItemGroupName, int Price, string Size) {
+            this.ItemGroupName = ItemGroupName;
+            this.Price = Price;
+            this.Size = Size;
+        }
+
+        public FictionalProduct() { }
+    }
+}
